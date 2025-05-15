501fe38301e2cc912d776c3e34ea5405c4a3cbaf
longjianfeis <77880019+longjianfeis@users.noreply.github.com>
2025-05-16 00:04:15 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,102 @@
+﻿const generatedBibEntries = {
+    "Abdollahpour2023Automatic": {
+        "author": "Mohammad Mahdi Abdollahpour and Mehrdad Ashtiani and Fatemeh Bakhshi",
+        "doi": "10.1007/s11219-023-09653-1",
+        "journal": "Software Quality Journal",
+        "keywords": "type:Deep Learning-Based Methods",
+        "pages": "1-30",
+        "title": "Automatic software code repair using deep learning techniques",
+        "type": "article",
+        "year": "2023"
+    },
+    "Allamanis2021Self-Supervised": {
+        "author": "Miltiadis Allamanis and Henry Jackson-Flux and Marc Brockschmidt",
+        "doi": "",
+        "keywords": "type:Self-Supervised Learning",
+        "pages": "27865-27876",
+        "title": "Self-Supervised Bug Detection and Repair",
+        "type": "article",
+        "url": "https://www.semanticscholar.org/reader/7f42e68c4caf74f91bf9990012efa8d7641235d6",
+        "year": "2021"
+    },
+    "Elmishali2018An": {
+        "author": "Amir Elmishali and Roni Stern and Meir Kalech",
+        "doi": "10.1016/J.ENGAPPAI.2017.12.011",
+        "journal": "Eng. Appl. Artif. Intell.",
+        "keywords": "type:Machine Learning-Based Methods",
+        "pages": "147-156",
+        "title": "An Artificial Intelligence paradigm for troubleshooting software bugs",
+        "type": "article",
+        "volume": "69",
+        "year": "2018"
+    },
+    "Li2020Improving": {
+        "author": "Yi Li",
+        "doi": "10.1145/3377812.3382172",
+        "journal": "2020 IEEE/ACM 42nd International Conference on Software Engineering: Companion Proceedings (ICSE-Companion)",
+        "keywords": "type:Deep Learning-Based Methods",
+        "pages": "137-139",
+        "title": "Improving Bug Detection and Fixing via Code Representation Learning",
+        "type": "article",
+        "year": "2020"
+    },
+    "Mahapatra2020Usage": {
+        "author": "Sumit Mahapatra and Subhankar Mishra",
+        "doi": "10.1007/978-3-030-38006-9_3",
+        "keywords": "type:Machine Learning-Based Methods",
+        "pages": "39-54",
+        "title": "Usage of Machine Learning in Software Testing",
+        "type": "article",
+        "year": "2020"
+    },
+    "Mutasim2023Leveraging": {
+        "author": "Ruba Mutasim and Gabriel Synnaeve and David Pichardie and Baptiste Rozi\u00e8re",
+        "doi": "10.48550/arXiv.2304.10379",
+        "journal": "ArXiv",
+        "keywords": "type:Hybrid Approaches",
+        "title": "Leveraging Static Analysis for Bug Repair",
+        "type": "article",
+        "volume": "abs/2304.10379",
+        "year": "2023"
+    },
+    "Ndlovu2024Software": {
+        "author": "Lungisani Ndlovu and Bheki Cossa and Caroline Makokoe and Lethukuthula Khumalo and Ntomfuthi Ntshangase and Siphelele Myaka and Nomalisa Ndhlovu and Mamello Mtshali",
+        "doi": "10.1109/ICECET61485.2024.10698379",
+        "journal": "2024 International Conference on Electrical, Computer and Energy Technologies (ICECET",
+        "keywords": "type:Review Papers",
+        "pages": "1-6",
+        "title": "Software Fault Detection Algorithms Using Artificial Intelligence: A Review and Classification",
+        "type": "article",
+        "year": "2024"
+    },
+    "Sangeetha2021Prediction": {
+        "author": "Y. Sangeetha and G. Lakshmi",
+        "doi": "10.1007/978-981-15-8221-9_251",
+        "keywords": "type:Machine Learning-Based Methods",
+        "pages": "2683-2692",
+        "title": "Prediction of Software Bugs Using Machine Learning Algorithm",
+        "type": "article",
+        "year": "2021"
+    },
+    "Sobania2023An": {
+        "author": "Dominik Sobania and Martin Briesch and Carol Hanna and J. Petke",
+        "doi": "10.1109/APR59189.2023.00012",
+        "journal": "2023 IEEE/ACM International Workshop on Automated Program Repair (APR)",
+        "keywords": "type:Large Language Models & Conversational AI",
+        "pages": "23-30",
+        "title": "An Analysis of the Automatic Bug Fixing Performance of ChatGPT",
+        "type": "article",
+        "year": "2023"
+    },
+    "Yousofvand2024Automatic": {
+        "author": "Leila Yousofvand and Seyfollah Soleimani and Vahid Rafe and S. Esfandyari",
+        "doi": "10.1007/s10462-023-10686-y",
+        "journal": "Artif. Intell. Rev.",
+        "keywords": "type:Deep Learning-Based Methods",
+        "pages": "39",
+        "title": "Automatic program bug fixing by focusing on finding the shortest sequence of changes",
+        "type": "article",
+        "volume": "57",
+        "year": "2024"
+    }
+};
